8205fa0dc2abd55c625598770f7529ec4db03bf8
Cláudia <claudiainescostamartins@gmail.com>
2020-03-13 15:33:11 +0000

    I02 working

--- I02/Client.java
+++ I02/Client.java
@@ -5,8 +5,14 @@ public class Client extends Thread {
     protected static MulticastSocket socket = null;
     protected static byte[] buf = new byte[256];
 
-    private static String mcast_addr; //IP address of the multicast group used by the server to advertise its service
-    private static int mcast_port; //port number of the multicast group used by the server to advertise its service
+    private static String mcast_addr; // IP address of the multicast group used by the server to advertise its service
+    private static int mcast_port; // port number of the multicast group used by the server to advertise its
+                                   // service
+
+    private static String oper;
+    private static String opnd;
+
+    private static String data;
 
     public static void main(String[] args) throws IOException {
         if (args.length != 4) {
@@ -16,25 +22,78 @@ public class Client extends Thread {
 
         mcast_addr = args[0];
         mcast_port = Integer.parseInt(args[1]);
+        oper = args[2];
+        opnd = args[3];
 
-        run1();
+        multicast();
+
+        service();
     }
 
-    public static void run1() {
+    public static void multicast() {
         try {
-            socket = new MulticastSocket(mcast_port); //4446
-            InetAddress group = InetAddress.getByName(mcast_addr); //"230.0.0.0"
+            socket = new MulticastSocket(mcast_port); // 4446
+            InetAddress group = InetAddress.getByName(mcast_addr); // "230.0.0.0"
             socket.joinGroup(group);
 
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
             String received = new String(packet.getData(), 0, packet.getLength());
 
-            System.out.println (received);
+            data = received;
+
+            System.out.println(received);
 
             socket.leaveGroup(group);
             socket.close();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void service() {
+        try {
+            byte[] sbuf;
+            String message;
+
+            // OPERAÇÃO DE REGISTO
+            if (oper.equals("register")) {
+                String[] parts = opnd.split(",");
+                String dns_name = parts[0];
+                String ip_address = parts[1];
+                message = "REGISTER " + dns_name + " " + ip_address;
+                sbuf = message.getBytes();
+            }
+            // OPERAÇÃO DE LOOKUP
+            else if (oper.equals("lookup")) {
+                message = "LOOKUP " + opnd;
+                sbuf = message.getBytes();
+            }
+            // NENHUMA OPERAÇÃO
+            else {
+                System.out.println("Usage: <oper> must be either lookup or register");
+                return;
+            }
+
+            String[] info = data.split(" ");
+
+            // send request
+            DatagramSocket socket = new DatagramSocket();
+            int port = Integer.parseInt(info[1]);
+            InetAddress address = InetAddress.getByName(info[0]);
+            DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, port);
+            socket.send(packet);
+
+            // get response
+            byte[] rbuf = new byte[256];
+            DatagramPacket rpacket = new DatagramPacket(rbuf, rbuf.length);
+            socket.receive(rpacket);
+            String response = new String(rbuf, 0, packet.getLength());
+
+            socket.close();
+
+            System.out.println("Client: " + oper + " " + opnd + " : " + response);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- I02/Server.java
+++ I02/Server.java
@@ -1,54 +1,147 @@
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 import java.io.IOException;
 import java.net.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        /*if (args.length != 3) {
+        if (args.length != 3) {
             System.out.println("Usage: java Server <srvc_port> <mcast_addr> <mcast_port> ");
             return;
-        }*/
+        }
 
-        //Multicast multicast = new Multicast(args);
+        Multicast multicast = new Multicast(args);
 
-        System.out.println("Usage: java Server <srvc_port> <mcast_addr> <mcast_port> ");
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(multicast, 1, 1, TimeUnit.SECONDS);
+
+        service(args[0]);
+    }
+
+    public static void service(String port_number) {
+
+        try {
+            // send request
+            DatagramSocket socket = new DatagramSocket(Integer.parseInt(port_number));
+            InetAddress address = InetAddress.getByName(port_number);
+
+            // lista de todas as entradas já gravadas
+            ArrayList<Pair> entries = new ArrayList<Pair>();
+
+            while (true) {
+                // get response
+                byte[] rbuf = new byte[256];
+                DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);
+                socket.receive(packet);
+                String received = new String(packet.getData());
+
+                System.out.println("Server: " + received);
+
+                String[] parts = received.split(" ");
+
+                String response = " ";
+
+                // REGISTER
+                if (parts.length == 3) {
+                    for (int i = 0; i < entries.size(); i++) {
+                        Pair obj = (Pair) entries.get(i);
+
+                        if (obj.getDns().equals(parts[1])) {
+                            response = "-1";
+                            break;
+                        }
+                    }
+
+                    if (!(response.equals("-1"))) {
+                        Pair pair = new Pair(parts[1], parts[2]);
+                        entries.add(pair);
+                        response = String.valueOf(entries.size());
+                    }
+                }
+
+                // LOOKUP
+                else if (parts.length == 2) {
+                    response = "NOT_FOUND";
+
+                    for (int i = 0; i < entries.size(); i++) {
+                        Pair obj = (Pair) entries.get(i);
+
+                        if (obj.getDns().equals(parts[1].trim())) {
+                            response = obj.getIp();
+                            break;
+                        }
+                    }
+                }
+
+                // send response
+                InetAddress radress = packet.getAddress();
+                int rport = packet.getPort();
+                byte[] rdata = response.getBytes();
+                DatagramPacket rpacket = new DatagramPacket(rdata, rdata.length, radress, rport);
+                socket.send(rpacket);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
-/*class Multicast implements Runnable{
+class Multicast implements Runnable {
     private static MulticastSocket socket;
     private static InetAddress group;
     private static byte[] buf;
     private static DatagramPacket packet;
 
-    private static String srvc_port; //port number where the server provides the service
-    private static String mcast_addr; //IP address of the multicast group used by the server to advertise its service
-    private static int mcast_port; //multicast group port number used by the server to advertise its service
+    private static String srvc_port; // port number where the server provides the service
+    private static String mcast_addr; // IP address of the multicast group used by the server to advertise its service
+    private static int mcast_port; // multicast group port number used by the server to advertise its service
 
     public Multicast(String[] args) throws IOException {
-        this.srvc_port = args[0];
-        this.mcast_addr = args[1];
-        this.mcast_port = Integer.parseInt(args[2]);
+        srvc_port = args[0];
+        mcast_addr = args[1];
+        mcast_port = Integer.parseInt(args[2]);
 
-        this.socket = new MulticastSocket();
+        socket = new MulticastSocket();
 
-        this.socket.setTimeToLive(1);
+        socket.setTimeToLive(1);
     }
 
     @Override
     public void run() {
         try {
-        this.group = InetAddress.getByName(this.mcast_addr); //"230.0.0.0"
+            group = InetAddress.getByName(mcast_addr); // "230.0.0.0"
 
-        String message = "localhost " + srvc_port;
-        this.buf = message.getBytes();
+            String message = "localhost " + srvc_port;
+            buf = message.getBytes();
 
-        this.packet = new DatagramPacket(this.buf, buf.length, this.group, this.mcast_port); //4446
-        this.socket.send(packet);
+            packet = new DatagramPacket(buf, buf.length, group, mcast_port); // 4446
+            socket.send(packet);
 
-        this.socket.close();
-        }
-        catch (IOException e) {
+            // this.socket.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}*/
+}
+
+class Pair {
+    private String dns_name;
+    private String ip_address;
+
+    public Pair(String dns_name, String ip_address) {
+        this.dns_name = dns_name;
+        this.ip_address = ip_address;
+    }
+
+    public String getIp() {
+        return ip_address;
+    }
+
+    public String getDns() {
+        return dns_name;
+    }
+}
