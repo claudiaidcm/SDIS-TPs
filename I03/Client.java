35cef03865185e5bffc0bd525d6f0c96367722e9
Cláudia <claudiainescostamartins@gmail.com>
2020-03-14 21:33:38 +0000

    Lab3 progress

---
+++ I03/Client.java
@@ -0,0 +1,25 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+
+        if(args.length != 4) {
+            System.out.println("Usage: java Server <host_name> <remote_object_name> <oper> <opnd> *");
+        }
+
+        String host = (args.length < 1) ? null : args[0];
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Hello stub = (Hello) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
